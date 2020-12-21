09e0162c20abc59eb2743d322d19bc44feb67b58
Jerome Dh <jdieuhou@yahoo.fr>
2020-12-21 04:44:23 +0100

    Small change

--- js/Utils.js
+++ js/Utils.js
@@ -398,7 +398,7 @@ function sendPlayerScoreOnline() {
 		fetch(SCORE_URL, {
 			method: 'POST',
 			headers: {
-				// 'Accept': 'application/json',
+				'Accept': 'application/json',
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify(player)
